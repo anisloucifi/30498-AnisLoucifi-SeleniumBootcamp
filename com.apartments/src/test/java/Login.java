12681372a040b9e1a165c47e1c8de361a25718ab
anisloucifi <louanis93@outlook.com>
2022-11-08 23:57:44 +0100

    second

--- com.apartments/src/test/java/Login.java
+++ com.apartments/src/test/java/Login.java
@@ -4,9 +4,6 @@ import org.testng.annotations.Test;
 public class Login extends BasePage {
     @Test
     public void login() {
-        HomePage hp = new HomePage();
-        String[][] data = excel.readStringArrays("log");
-        hp.doSignIn(data[0][0],data[0][1]);
 
 
 
